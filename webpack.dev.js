bef30719b20df8bffc76cd27f3fc1b32bcf7b916
Angel <angel.e.diaz.b@gmail.com>
2021-03-24 15:12:59 -0400

    Boilerplate first commit

---
+++ webpack.dev.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css",
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+  },
+};
